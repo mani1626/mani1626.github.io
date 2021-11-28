c454f20255fa52aa31ec68555eb3bba0878dde0f
MayankVats <vats.2610@gmail.com>
2021-11-28 09:05:37 +0530

    update

--- script.js
+++ script.js
@@ -34,18 +34,183 @@ setInterval(() => {
     "#da22ff",
     "#5FC3E4",
     "#E55D87",
+    "#3CA55C",
+    "#B5AC49",
+    "#F4D03F",
   ];
   let body = document.querySelector("body");
 
-  let colorOne = colors[+(Math.random() * 6).toFixed()];
-  let colorTwo = colors[+(Math.random() * 6).toFixed()];
+  let range = colors.length;
+  let colorOne = colors[+(Math.random() * range).toFixed()];
+  let colorTwo = colors[+(Math.random() * range).toFixed()];
 
   while (colorOne === colorTwo) {
-    colorOne = colors[+(Math.random() * 6).toFixed()];
-    colorTwo = colors[+(Math.random() * 6).toFixed()];
+    colorOne = colors[+(Math.random() * range).toFixed()];
+    colorTwo = colors[+(Math.random() * range).toFixed()];
   }
 
   let direction = +(Math.random() * 360).toFixed();
 
   body.style.background = `linear-gradient(${direction}deg, ${colorOne}, ${colorTwo})`;
 }, 900);
+
+/* */
+
+function displayFireworks() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    var particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
+}
+
+function basicCanon() {
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+}
+
+function realisticLook() {
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 },
+  };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
+
+function snow() {
+  var duration = 15 * 1000;
+  var animationEnd = Date.now() + duration;
+  var skew = 1;
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  (function frame() {
+    var timeLeft = animationEnd - Date.now();
+    var ticks = Math.max(200, 500 * (timeLeft / duration));
+    skew = Math.max(0.8, skew - 0.001);
+
+    confetti({
+      particleCount: 1,
+      startVelocity: 0,
+      ticks: ticks,
+      origin: {
+        x: Math.random(),
+        // since particles fall down, skew start toward the top
+        y: Math.random() * skew - 0.2,
+      },
+      colors: ["#ffffff"],
+      shapes: ["circle"],
+      gravity: randomInRange(0.4, 0.6),
+      scalar: randomInRange(0.4, 1),
+      drift: randomInRange(-0.4, 0.4),
+    });
+
+    if (timeLeft > 0) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+function buckEyes() {
+  var end = Date.now() + 8 * 1000;
+
+  // go Buckeyes!
+  var colors = ["#bb0000", "#ffffff"];
+
+  (function frame() {
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: colors,
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: colors,
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+}
+
+const confettiObject = {
+  displayFireworks,
+  basicCanon,
+  realisticLook,
+  snow,
+  buckEyes,
+};
+
+function randomConfetti() {
+  const confetti = Object.keys(confettiObject)[+(Math.random() * 5).toFixed()];
+  confettiObject[confetti].call();
+}
+
+const btn = document.querySelector("body");
+btn.addEventListener("click", randomConfetti);
